4cde847ecb25325b1b5b61313697536542c3d018
Natallia Paliukhovich-Karastsialiova <n.paliukhovich98@gmail.com>
2025-08-23 17:07:18 +0500

    feat: add initial implementation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,54 @@
-import './scss/styles.scss';
+import { Api } from "./components/base/Api";
+import { APIManager } from "./components/communication/APIManager";
+import { Buyer } from "./components/models/Buyer";
+import { Catalog } from "./components/models/Catalog";
+import { Cart } from "./components/models/Сart";
+import "./scss/styles.scss";
+import { API_URL } from "./utils/constants";
+import { apiProducts } from "./utils/data";
+
+const productsCatalog = new Catalog();
+productsCatalog.products = apiProducts.items;
+console.log("Массив товаров из каталога: ", productsCatalog.products);
+
+const firstId = productsCatalog.products[0]?.id;
+console.log("Первый товар по id:", productsCatalog.getProductById(firstId));
+productsCatalog.selectedProduct = productsCatalog.products[0];
+console.log("Выбранный товар:", productsCatalog.selectedProduct);
+
+const cart = new Cart();
+console.log("Корзина при инициализации:", cart.products);
+
+cart.addProduct(productsCatalog.products[0]);
+console.log("После добавления товара:", cart.products);
+console.log("Количество товаров в корзине:", cart.size);
+console.log("Общая сумма корзины:", cart.total);
+console.log("Есть ли товар с id", firstId, "?", cart.isExist(firstId));
+
+cart.deleteProduct(firstId!);
+console.log("После удаления товара:", cart.products);
+console.log("Корзина пуста?", cart.size === 0);
+
+const buyer = new Buyer(
+  "card",
+  "Ташкент, ул. Ленина, 1",
+  "test@gmail.com",
+  "+12345678"
+);
+
+console.log("Информация о пользователе:", buyer.userInfo);
+console.log("Пользователь валиден?", buyer.isValid());
+
+buyer.email = "new@mail.com";
+console.log("После изменения email:", buyer.userInfo);
+
+buyer.clearInfo();
+console.log("После очистки данных:", buyer.userInfo);
+console.log("Пользователь валиден после очистки?", buyer.isValid());
+
+const API = new Api(API_URL);
+const apiManager = new APIManager(API);
+
+const loadedProducts = await apiManager.fetchProducts();
+productsCatalog.products = loadedProducts.items;
+console.log("Массив товаров из API: ", productsCatalog.products);
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,44 @@
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+export type ApiPostMethods = "POST" | "PUT" | "DELETE";
 
 export interface IApi {
-    get<T extends object>(uri: string): Promise<T>;
-    post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+  get<T extends object>(uri: string): Promise<T>;
+  post<T extends object>(
+    uri: string,
+    data: object,
+    method?: ApiPostMethods
+  ): Promise<T>;
+}
+
+export interface IProduct {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  category: string;
+  price: number | null;
+}
+export interface IBuyer {
+  payment: "cash" | "card" | "";
+  email: string;
+  phone: string;
+  address: string;
+}
+
+export interface IProductsAPIResponse {
+  total: number;
+  items: IProduct[];
+}
+
+export interface IOrder extends IBuyer {
+  total: number;
+  items: string[];
+}
+
+export interface IOrderResult {
+  id: string;
+  total: number;
+}
+
+export interface IOrderFailed {
+  error: string;
 }
